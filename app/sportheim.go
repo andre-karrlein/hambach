2802730f31293d9f424354c366ef9ad4e765ad4c
André Karrlein <andre@karrlein.com>
2023-02-17 09:11:23 +0100

    add missing function

--- app/sportheim.go
+++ app/sportheim.go
@@ -93,3 +93,23 @@ func (sportheim *sportheim) OnNav(ctx app.Context) {
 		sportheim.Update()
 	})
 }
+
+func chunksOfStrings(slice []string, chunkSize int) [][]string {
+	var chunks [][]string
+	for {
+		if len(slice) == 0 {
+			break
+		}
+
+		// necessary check to avoid slicing beyond
+		// slice capacity
+		if len(slice) < chunkSize {
+			chunkSize = len(slice)
+		}
+
+		chunks = append(chunks, slice[0:chunkSize])
+		slice = slice[chunkSize:]
+	}
+
+	return chunks
+}
