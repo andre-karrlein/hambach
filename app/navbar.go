37cc4b59a5db5087c83b9fc425a04e5db7e59be6
André Karrlein <andre@karrlein.com>
2023-05-16 17:08:12 +0200

    Update navbar.go

--- app/navbar.go
+++ app/navbar.go
@@ -68,6 +68,9 @@ func (n *navbar) Render() app.UI {
 							"Freizetigruppe",
 						).Href("/article/32"),
 						app.A().Class("navbar-item").Text(
+							"Fitness und Gesundheit",
+						).Href("/article/09bb29a6-7eb2-4f80-90c7-c2fc414a05f"),
+						app.A().Class("navbar-item").Text(
 							"Hallenbelegung",
 						).Href("/article/87"),
 					),
